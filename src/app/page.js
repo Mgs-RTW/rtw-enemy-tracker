39e23b77de40a0dcd0ad7a7d6b52451040ee2d57
Anders <>
2023-09-09 09:51:32 +0200

    save

--- src/app/page.js
+++ src/app/page.js
@@ -102,7 +102,7 @@ export default function Home({ config }) {
               id="search-box"
               className="search-box"
               type="text"
-              placeholder="Just enter player name..."
+              placeholder="Player name..."
               onChange={processChange.bind(this)}
             />
           </Card>
